1625f07e49492ecc63a50351112e1a3d1c57cc3c
lesa3111 <amael.le.squin@usherbrooke.ca>
2020-08-19 17:18:43 -0400

    Script adapted to raster cropped with an extent. Writing function adapting to data.table

--- createLandscape/landscapeForCpp.R
+++ createLandscape/landscapeForCpp.R
@@ -39,6 +39,23 @@ writeCppClimate = function(climate, cppNames, id, crs, sep = " = ", rm = FALSE)
 	sink(file = NULL)
 }
 
+## Tu use multi threading from data.table
+writeCppClimate_DT = function(climateNamedVector, cppNames, id, crs, sep = " = ", rm = FALSE)
+{
+	outfileName = paste0("./climate_", id, ".txt")
+	if (rm & file.exists(outfileName))
+		file.remove(outfileName)
+
+	sink(file = outfileName, append = TRUE)
+	for (i in 1:length(cppNames))
+		cat(paste0(cppNames[i], sep, climateNamedVector[cppNames[i]]), sep = "\n")
+
+	cat(paste0("plotArea", sep, 400), sep = "\n") # To modify!!!
+	cat(paste0("proj4string", sep, crs), sep = "\n") # To modify!!!
+
+	sink(file = NULL)
+}
+
 #### Common variables
 ## Folders
 loadPath = "~/projects/def-dgravel/amael/article1/progToSendToReview/"
@@ -69,20 +86,50 @@ clim = climate[dist == min(dist), ]
 #### Write file for Cpp
 writeCppClimate(clim, cppNames, id = 1, crs, sep = " = ", rm = TRUE)
 
-# ####
-# ## Load raster
-# climate_rs = raster(paste0(loadPath, "clim60sec/clim_2010.grd"))
+#### Get climate within a disc
+## Transform dist in m to km
+climate[, dist := dist/1000]
 
-# ## Transform
-# climate_rs = projectRaster(climate_rs, crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-# dir.create("./reprojRast")
-# writeRaster(climate_rs, filename = "reprojRast/clim_2010.grd", bandorder = "BIL")
+clim = climate[dist < 5]
 
+for (id in 1:clim[, .N])
+	writeCppClimate(clim[id], cppNames, id, crs, sep = " = ", rm = TRUE)
 
-# ## Define crop extent
-# bottomLeft
-# topLeft
-# topRight
-# bottomRight
+#### Get climate from raster
+## Load raster
+climate_rs = stack(paste0(loadPath, "clim60sec/clim_2010.grd"))
 
-# crop_extent = SpatialPoints()
+# ## Transform raster if not done yet
+# if (!file.exists("./reprojRast/clim_2010.grd"))
+# {
+# 	#Load raster
+# 	climate_rs = raster(paste0(loadPath, "clim60sec/clim_2010.grd"))
+
+# 	# Transform
+# 	climate_rs = projectRaster(climate_rs, crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+# 	dir.create("./reprojRast")
+# 	writeRaster(climate_rs, filename = "reprojRast/clim_2010.grd", bandorder = "BIL")
+# }
+
+## Define crop extent
+lonMin = -74
+lonMax = -73.9
+latMin = 41
+latMax = 41.1
+
+crop_extent = extent(c(lonMin, lonMax, latMin, latMax))
+crop_extent = projectExtent(raster(crop_extent, crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"),
+	crs(climate_rs))
+
+croppedClimate = crop(climate_rs[[c("bio60_01", "bio60_06", "bio60_12", "bio60_17")]], crop_extent)
+names(croppedClimate) = c("annual_mean_temperature", "min_temperature_of_coldest_month",
+	"annual_precipitation", "precipitation_of_driest_quarter")
+
+vals = as.data.table(rasterToPoints(croppedClimate))
+setnames(vals, old = c("x", "y"), new = c("latitude", "longitude"))
+
+for (id in 1:vals[, .N])
+	writeCppClimate(vals[id], cppNames, id, crs, sep = " = ", rm = TRUE)
+
+vals[, id := 1:.N]
+vals[, writeCppClimate_DT(unlist(vals[id]), cppNames, id, crs, sep = " = ", rm = TRUE), by = id]
