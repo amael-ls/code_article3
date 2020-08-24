31a71892eb991aab46d0281e6d02b1eb710ff415
lesa3111 <amael.le.squin@usherbrooke.ca>
2020-08-24 11:55:50 -0400

    Output saved in files

--- createIC/randomGenerator.R
+++ createIC/randomGenerator.R
@@ -1,47 +1,51 @@
 
-#### Aim of prog: Generate n random numbers such as their sum is alpha
+#### Aim of prog: Generate n random cohorts such that basal areas equal alpha
 ## Comments
 # This program generates n random densities of cohort and their associated dbh, such that the basal area is
-# alpha m^2/ha = alpha x 10e-4 (dimensionless number).
+# alpha m^2/ha
 # It is using a dirichlet distribution which generates n numbers that sum to 1.
 # Multiply the output by alpha to get a sum to the desired BA
 # For instance, to generate a basal area 25 m^2/ha = 2.5e-3 (dimensionless number)
 # with 150 individuals we would do:
-#	- Generate 150 dbh
-#	- Generate 150 numbers using dirichlet distribution: β_i = N_i * π * dbh_i^2/(4 . 10^4) such that the sum is alpha
-#	- From them, deduce N_i
+#	- Generate 150 dbh (in cm)
+#	- Generate 150 numbers β using dirichlet distribution: β_i = N_i * π * dbh_i^2/(4 . 10^4) such that the sum is alpha
+#	- From β_i, deduce N_i
 #	- Check that Σ N_i * π * dbh_i^2/(4 . 10^4) = alpha
 #
 ## Output:
 #	- The output is a matrix of size nbPlots x nbCohorts
 #	- Each line is for one plot
-#	- Each column is associated to a dbh (i.e., same structure among plots)
+#	- Each column is associated to a dbh (i.e., same size-structure among plots)
 #
 # See https://reference.wolfram.com/language/ref/DirichletDistribution.html for more explanation on this distribtion
 #
 
 #### Load library and clear memory
-library(rBeta2009) # If you cannot download it, check MCMCpack
+library(rBeta2009) # If you cannot download it, MCMCpack also have a dirichlet random generator
 
 rm(list = ls())
 graphics.off()
 
 #### Tool function
-printIC(densities, dbh)
+printIC = function(densities, dbh, path, filenamePattern, sep = " ", reset = TRUE)
 {
 	nbCohorts = ncol(densities)
 	nbPlots = nrow(densities)
 	if (length(dbh) != nbCohorts)
 		stop(paste0("The number of cohorts (", nbCohorts, ") should be the same number of dbh provided (", length(dbh), ")"))
 
-	for (i in 1:nrow(croppedClimate_rs))
+	for (plot in 1:nbPlots)
 	{
-		sink(file = paste0(savingPath, metadata_fileName), append = TRUE)
+		file = paste0(path, filenamePattern, plot, ".txt")
 
-		cat(paste0("nRow", sep, m_nRow, sep = "\n"))
-		cat(paste0("nCol", sep, m_nCol, sep = "\n"))
-		cat(paste0("path", sep, m_path), sep = "\n")
-		cat(paste0("delimiter", sep, m_delimiter), sep = "\n")
+		if (reset & file.exists(file))
+			file.remove(file)
+
+		sink(file = file, append = TRUE)
+		cat(paste0("density", sep, "dbh", sep = "\n"))
+
+		for (cohort in 1:nbCohorts)
+			cat(paste0(densities[plot, cohort], sep, dbh[cohort], sep = "\n"))
 
 		sink(file = NULL)
 	}
@@ -75,3 +79,15 @@ for (i in 1:nbPlots)
 for (i in 1:nbPlots)
 	if (!all.equal((dbh^2 %*% t(densities)[,i]*pi/(4*10^4))[1,1], BA))
 		print(paste0("Check row ", i, ". The basal area of this plot might not be the value expected"))
+
+#### Write files initial condition
+## Parameters
+path = "./randomInitialCondition/"
+filenamePattern = "ic_"
+
+## Create folder
+if (!dir.exists(path))
+	dir.create(path)
+
+## Write files
+printIC(densities, dbh, path, filenamePattern, sep = " ", reset = TRUE)
