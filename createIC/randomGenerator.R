0e52e0d0509364fdcbf41a91014cc1026ffe19f7
lesa3111 <amael.le.squin@usherbrooke.ca>
2020-09-01 21:27:09 -0400

    rowsSum to check number of trees. dbh vs dbh_meter to check

--- createIC/randomGenerator.R
+++ createIC/randomGenerator.R
@@ -7,7 +7,7 @@
 # Multiply the output by alpha to get a sum to the desired BA
 # For instance, to generate a basal area 25 m^2/ha = 2.5e-3 (dimensionless number)
 # with 150 individuals we would do:
-#	- Generate 150 dbh (in cm)
+#	- Generate 150 dbh (if in cm, convert to meters)
 #	- Generate 150 numbers β using dirichlet distribution: β_i = N_i * π * dbh_i^2/(4 . 10^4) such that the sum is alpha
 #	- From β_i, deduce N_i
 #	- Check that Σ N_i * π * dbh_i^2/(4 . 10^4) = alpha
@@ -24,6 +24,7 @@
 #
 
 #### Load library and clear memory
+library(data.table)
 library(rBeta2009) # If you cannot download it, MCMCpack also have a dirichlet random generator
 
 rm(list = ls())
@@ -78,19 +79,24 @@ BA = 25 # m^2/ha
 ## Generate beta numbers (read comments at the beginning)
 betas = BA*rdirichlet(nbPlots, alphaShape)
 
-## Generate dbh
+## Generate dbh (in cm)
 dbh = sample(x = minDiameter:maxDiameter, size = nbCohorts, replace = TRUE)
+
+dbh_meters = dbh/100
 
 ## Deduce densities N
 densities = matrix(data = 0, nrow = nrow(betas), ncol = ncol(betas))
 for (i in 1:nbPlots)
 	for(j in 1:nbCohorts)
-		densities[i, j] = betas[i, j]*4*10^4/(pi*dbh[j]^2)
+		densities[i, j] = betas[i, j]*4*10^4/(pi*dbh_meters[j]^2)
 
 ## Check cohorts basal area
 for (i in 1:nbPlots)
-	if (!all.equal((dbh^2 %*% t(densities)[,i]*pi/(4*10^4))[1,1], BA))
+	if (!all.equal((dbh_meters^2 %*% t(densities)[,i]*pi/(4*10^4))[1,1], BA))
 		print(paste0("Check row ", i, ". The basal area of this plot might not be the value expected"))
+
+## Number of trees per patch
+data.table(id_plots, density_ha = rowSums(densities))
 
 #### Write files initial condition
 ## Create folder
