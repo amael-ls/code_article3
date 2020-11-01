b278ee09713cd771d9f98cd6a380d73c29500630
lesa3111 <amael.le.squin@usherbrooke.ca>
2020-11-01 17:40:05 -0500

    Added analytical_results.R to analyse 4 special cases that can be compared to analytical solutions

---
+++ analyses/analytical_results.R
@@ -0,0 +1,12 @@
+
+#### Aim of prog:  Create initial condition for analytical cases:
+# Case 1: No dispersal, no fecundity, no competition. Path = ic_1/
+# Case 2: No dispersal, no fecundity. Path = ic_2/
+# Case 3: No dispersal, no competition. Path = ic_3/
+# Case 4: No dispersal. Path = ic_4/
+
+#### Load library and clear memory
+library(data.table)
+
+rm(list = ls())
+graphics.off()
