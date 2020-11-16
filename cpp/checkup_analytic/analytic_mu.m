d27534866e53c4848c0b0e5dfde8ccf5eee88220
lesa3111 <amael.le.squin@usherbrooke.ca>
2020-11-16 23:07:30 +0100

    A updated test was done in the main program. The analytical calculus are in the 2nd notebook Ph.D.

--- cpp/checkup_analytic/analytic_mu.m
+++
@@ -1,8 +0,0 @@
-
-%%%% Aim of prog: Find analytic solution of mu
-%% Declare variables
-syms mu(t) s_m;
-ode = diff(mu, t) == (2 + sqrt(1 - 2*log(exp(d*t) + exp(-s_m^2/2 - s_m))) - 1)*exp(-(sqrt(1 - 2*log(exp(d*t) + exp(-s_m^2/2 - s_m))) - 1))/(1 + mu);
-
-cond = mu(0) == 2;
-mu_sol(t) = dsolve(ode, cond);
--- cpp/checkup_analytic/d.m
+++
@@ -1,9 +0,0 @@
-function [ out ] = d( t, s )
-	% d is negative here, to get a real solution for s*. Although the density
-	% eta will diverge in time (since a negative mortality becomes a source!),
-	% eta and the other integrals are still bounded in size. It implies that I
-	% will integrate only on a bounded time domain.
-	% Not that because I know d is constant, I do not add the parameter s*
-	out = -0.4; % cf Demography.h++
-
-end
