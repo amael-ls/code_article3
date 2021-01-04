1add4cbae48168413280da28c2121413e6fc0251
lesa3111 <amael.le.squin@usherbrooke.ca>
2021-01-04 17:42:24 +0100

    Updated folders to read data

--- plotDemography/averageGrowthMortality.m
+++ plotDemography/averageGrowthMortality.m
@@ -1,34 +1,20 @@
-%% Clear console and variables
-% clear all
-% clc
-
-disp('Matlab main.m is starting')
-
-%% Parallel pool cluster
-% create a local cluster object
-pc = parcluster('local');
-
-% explicitly set the JobStorageLocation to the temp directory that was
-% created in the sbatch script submission.sh
-pc.JobStorageLocation = strcat('/scratch/amael/', getenv('SLURM_JOB_ID'));
-
-slurm_cpus = str2num(getenv('SLURM_CPUS_PER_TASK'));
-
-parpool(pc, str2num(getenv('SLURM_CPUS_PER_TASK')))
 
 %% Load data
 % --- Allometries
-C0_C1 = readtable('../createMatlabData/C0_C1.csv');
-allometries = readtable('../createMatlabData/purves2007_allometries.csv');
+C0_C1 = readtable('~/projects/def-dgravel/amael/article1/progToSendToReview/createMatlabData/C0_C1.csv');
+allometries = readtable('~/projects/def-dgravel/amael/article1/progToSendToReview/createMatlabData/purves2007_allometries.csv');
 
 C0_C1.Properties.RowNames = C0_C1.parameter;
 allometries.Properties.RowNames = allometries.species;
 
 % --- Species names
-currentSpecies = '28728-ACE-RUB';
+currentSpecies = '28731-ACE-SAC';
+
+% --- Foder climate
+landscapeSize = 'climate_200x7';
 
 % --- Species-specific integral bounds (dbh corresponding to 45m height)
-integral_bounds = readtable('../createMatlabData/dbh_params.csv');
+integral_bounds = readtable('~/projects/def-dgravel/amael/article1/progToSendToReview/createMatlabData/dbh_params.csv');
 integral_bounds.Properties.RowNames = integral_bounds.species_id;
 
 %% Growth function to compute size at age_max. Autonomous ODE, but 't' is required by ODE45
@@ -37,18 +23,18 @@ growth_fct = @(t, dbh, c0, c1, c2, scmu_g, scsd_g, scmu_dbh, scsd_dbh) ...
 	c1*(dbh - scmu_dbh)/scsd_dbh + ...
 	c2*((dbh - scmu_dbh)/scsd_dbh)^2) + scmu_g);
 
-mortality_fct = @(dbh, c0, c1, c2, scmu_dbh, scsd_dbh) 1 - ...
-	exp(-exp(c0 + c1*(dbh - scmu_dbh)/scsd_dbh + c2*((dbh - scmu_dbh)/scsd_dbh)^2));
+mortality_fct = @(dbh, c0, c1, c2, scmu_dbh, scsd_dbh) ...
+	exp(c0 + c1*(dbh - scmu_dbh)/scsd_dbh + c2*((dbh - scmu_dbh)/scsd_dbh)^2);
 	
 %% Run
 % --- Species-specific parameters and data
 disp(['species id: ', currentSpecies])
 
-scalingGrowth = readtable('../createMatlabData/growthScaling.csv');
+scalingGrowth = readtable('~/projects/def-dgravel/amael/article1/progToSendToReview/createMatlabData/growthScaling.csv');
 scalingGrowth.Properties.RowNames = scalingGrowth.species_id;
 scalingGrowth = scalingGrowth(currentSpecies, {'mu', 'sd'});
 
-dbh_scalingGrowth = readtable('../createMatlabData/growthDbhScaling.csv');
+dbh_scalingGrowth = readtable('~/projects/def-dgravel/amael/article1/progToSendToReview/createMatlabData/growthDbhScaling.csv');
 dbh_scalingGrowth.Properties.RowNames = dbh_scalingGrowth.species_id;
 dbh_scalingGrowth = dbh_scalingGrowth(currentSpecies, {'mu', 'sd'});
 
@@ -57,27 +43,29 @@ sd_g = scalingGrowth.sd;
 mu_dbh_g = dbh_scalingGrowth.mu;
 sd_dbh_g = dbh_scalingGrowth.sd;
 
-dbh_scalingMortality = readtable('../createMatlabData/mortalityDbhScaling.csv');
+dbh_scalingMortality = readtable('~/projects/def-dgravel/amael/article1/progToSendToReview/createMatlabData/mortalityDbhScaling.csv');
 dbh_scalingMortality.Properties.RowNames = dbh_scalingMortality.species_id;
 dbh_scalingMortality = dbh_scalingMortality(currentSpecies, {'mu', 'sd'});
 
 mu_dbh_m = dbh_scalingMortality.mu;
 sd_dbh_m = dbh_scalingMortality.sd;
 
-climate_over_g = readtable(char(strcat('../R0/Matlab_data/', currentSpecies, '/matlabGrowth_above.csv')));
+climate_over_g = readtable(char(strcat('./', landscapeSize, '/matlabGrowth_above.csv')));
 climate_over_g = climate_over_g(:, {'beta0', 'beta1', 'beta2'});
-climate_under_g = readtable(char(strcat('../R0/Matlab_data/', currentSpecies, '/matlabGrowth_below.csv')));
+climate_under_g = readtable(char(strcat('./', landscapeSize, '/matlabGrowth_below.csv')));
 climate_under_g = climate_under_g(:, {'beta0', 'beta1', 'beta2'});
 
-climate_over_m = readtable(char(strcat('../R0/Matlab_data/', currentSpecies, '/matlabMortality_above.csv')));
+climate_over_m = readtable(char(strcat('./', landscapeSize, '/matlabMortality_above.csv')));
 climate_over_m = climate_over_m(:, {'beta0', 'beta1', 'beta2'});
-climate_under_m = readtable(char(strcat('../R0/Matlab_data/', currentSpecies, '/matlabMortality_below.csv')));
+climate_under_m = readtable(char(strcat('./', landscapeSize, '/matlabMortality_below.csv')));
 climate_under_m = climate_under_m(:, {'beta0', 'beta1', 'beta2'});
 
 s_star = integral_bounds(currentSpecies, 'dbh_star10').dbh_star10;
 s_inf = integral_bounds(currentSpecies, 'dbh_inf').dbh_inf;
 age_max = integral_bounds(currentSpecies, 'age_max').age_max;
-local_s = readtable(char(strcat('../R0/results/', currentSpecies, '/local_s_inf.csv')), 'ReadVariableNames', false);
+
+%! RESTART HERE !!!!
+local_s = readtable(char(strcat('~/projects/def-dgravel/amael/article1/progToSendToReview/R0/results/', currentSpecies, '/local_s_inf.csv')), 'ReadVariableNames', false);
 local_s = local_s.Var1;
 
 allometries = allometries(currentSpecies, {'a', 'b', 'T'});
@@ -111,7 +99,7 @@ parfor (j = 1:n)
 	local_s_inf = local_s(j);
 	current_s_inf = min(local_s_inf, s_inf);
 
-	% If s_inf < s_star, then integrate up to s_inf only. Not that I set t = 0 (its value does not matter)
+	% If s_inf < s_star, then integrate up to s_inf only. Not that I set t = 0 (its value does not matter)
 	if current_s_inf < s_star
 		averageGrowth(j) = 1/current_s_inf * integral( @(x) growth_fct(0, x, fixef_growth_under.beta0, fixef_growth_under.beta1, fixef_growth_under.beta2, mu_g, sd_g, mu_dbh_g, sd_dbh_g), 0, current_s_inf, 'ArrayValued', true);
 
