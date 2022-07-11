a02c476622b579b4f06a3e9fd7e99563189e75c9
lesa3111 <amael.lesquin@gmail.com>
2022-07-11 17:52:58 +0200

    Tracking helper.sh: create directories automatically

---
+++ helpers/helper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#### Check number of arguments
+if [[ "$#" -ne 1 ]]
+then
+	echo "You must specify the file containing the simulation parameters"
+	exit
+fi
+
+#### Read file and create directories if necessary
+input="$1"
+while IFS='' read -r line
+do
+	key=${line%=*}
+	key=$(sed "s/ //g" <<< $key)
+	if [[ "$key" =~ ^(species_path|initPath|summaryFilePath|popDynFilePath)$ ]]
+	then
+		value=${line##*=}
+		value=$(sed "s/ //g" <<< $value)
+
+		if [[ -d "$value" ]]
+		then
+			echo "$value already exists on your filesystem. Nothing was done"
+		else
+			echo "Creating directory $value"
+			mkdir -p $value
+		fi
+	fi
+done < "$input"
