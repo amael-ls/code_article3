bde758ca56114fd5296ee5eb0881a073be71d737
lesa3111 <amael.lesquin@gmail.com>
2022-07-15 12:48:58 +0200

    Updated the projection, fills NA, added a larger landscape 300*11 that will be used for the simulations. THIS SCRIPT SHOULD BE CLARIFIED

--- code_helpers/landscapeForCpp.R
+++ code_helpers/landscapeForCpp.R
@@ -9,13 +9,15 @@
 #		- precipitation_of_driest_quarter (mortality)
 #
 ## Remarks:
-# The climatic data are stored in the article 1 folder on Beluga (Compute Canada)
+#	1. The raw climatic data are stored in ../raw_data
+#	2. I use the projection EPSG:3799 simply because it is easier to count with meters than degrees. It is quite precise for Quebec
+#		For another region, it would worth taking something else
 #
 
 #### Load packages
 library(data.table)
 library(stringi)
-library(raster)
+library(terra)
 
 #### Clear memory and graphs
 rm(list = ls())
@@ -46,8 +48,8 @@ writeCppClimate_DT = function(climateNamedVector, cppNames, id, crs, deltaX, del
 
 #### Common variables
 ## Folders
-loadPath = "~/projects/def-dgravel/amael/article1/progToSendToReview/"
-outputPath = "./climate_200x7_abba/"
+loadPath = "../raw_data/"
+outputPath = "../run/data/landscape_300x7/"
 
 if (!dir.exists(outputPath))
 	dir.create(outputPath)
@@ -69,16 +71,18 @@ nbRefugia = 10
 
 #### Get climate from raster
 ## Load raster
-climate_rs = stack(paste0(loadPath, "clim60sec/clim_2010.grd"))
+ls_variables = list.dirs(path = loadPath, recursive = FALSE, full.names = FALSE)
+ls_rast = paste0(loadPath, ls_variables, "/2010_",
+	rep(c("quarter_min.tif", "annual_min.tif", "annual_mean.tif"), each = length(ls_variables)))
+climate_rs = rast(ls_rast)
 
-# Projection
-crs = crs(climate_rs, asText = TRUE)
+names(climate_rs) = paste0(ls_variables, "_", rep(c("quarter_min", "annual_min", "annual_mean"), each = length(ls_variables)))
 
 ## Define crop extent number 1 (Mont-Orford National Park):
-lonMin = -72.2117
-lonMax = -72.1117
-latMin = 45.3179
-latMax = 45.5179
+lonMin = -72.207962
+lonMax = -72.107962
+latMin = 45.345086
+latMax = 45.545086
 
 ## Define crop extent number 2 (Old Tappan Borough, NJ 07675, USA):
 # lonMin = -74
@@ -86,47 +90,56 @@ latMax = 45.5179
 # latMin = 41
 # latMax = 41.2
 
-crop_extent = extent(c(lonMin, lonMax, latMin, latMax))
-crop_extent = projectExtent(raster(crop_extent, crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"),
-	crs(climate_rs))
+crop_extent = ext(c(lonMin, lonMax, latMin, latMax))
 
-croppedClimate = crop(climate_rs[[c("bio60_01", "bio60_06", "bio60_12", "bio60_17")]], crop_extent)
+croppedClimate = crop(climate_rs[[c("tas_annual_mean", "tas_annual_min", "pr_annual_mean", "pr_quarter_min")]], crop_extent)
+
 names(croppedClimate) = c("annual_mean_temperature", "min_temperature_of_coldest_month",
 	"annual_precipitation", "precipitation_of_driest_quarter")
+
+# Projection
+croppedClimate = project(x = croppedClimate, "epsg:3799")
+
+crs_str = crs(croppedClimate, describe = TRUE)[c("name", "authority", "code")]
+crs_str = paste0(crs_str["name"], ", ", crs_str["authority"], ":", crs_str["code"])
 
 nrows = nrow(croppedClimate)
 ncols = ncol(croppedClimate)
 
-deltaX = xres(croppedClimate) # 1060
-deltaY = yres(croppedClimate) # 1830
+deltaX = xres(croppedClimate)
+deltaY = yres(croppedClimate)
+
+## Fill the gaps created by the projection (no idea why...)
+croppedClimate = focal(croppedClimate, w = matrix(data = c(1, 1, 1, 1, 0, 1, 1, 1, 1), nrow = 3),
+	fun = mean, na.policy = "only", na.rm = TRUE)
 
 ## Change resolution to a 20 x 20 m
 downScale_factors = floor(res(croppedClimate)/20)
-croppedClimate = disaggregate(x = croppedClimate, fact = downScale_factors, method = "bilinear")
+croppedClimate = disagg(x = croppedClimate, fact = downScale_factors, method = "bilinear")
 
 #! --- Crash test zone, to have a smaller landscape
 #? What follows is for the crop extent number 1 (Mont-Orford National Park)
-# For a 200 x 7 landscape
-crop_extent = extent(c(2111758, 2111898, 406431.3, 410453.3)) # order = xmin, xmax, ymin, ymax
+# For a 300 x 11 landscape
+crop_extent = ext(c(626881, 627106, 151133, 157253)) # order = xmin, xmax, ymin, ymax
 
 #? What follows is for the crop extent number 2 (Old Tappan Borough, NJ 07675, USA)
 # # For a 100 x 100 landscape
-# crop_extent = extent(c(2135078, 2137078, -93158, -91146)) # order = xmin, xmax, ymin, ymax
+# crop_extent = extent(c(xxx, xxx, xxx, xxx))
 
 # # For a 200 x 7 landscape
-# crop_extent = extent(c(2135078, 2135218, -93158, -89136))
+# crop_extent = extent(c(xxx, xxx, xxx, xxx))
 
 # # For a 100 x 5 landscape
-# crop_extent = extent(c(2135078, 2135178, -93158, -91146))
+# crop_extent = extent(c(xxx, xxx, xxx, xxx))
 
 # # For a 10 x 100 landscape
-# crop_extent = extent(c(2135078, 2137078, -93158, -92957))
+# crop_extent = extent(c(xxx, xxx, xxx, xxx))
 
 # # For a 20 x 20 landscape
-# crop_extent = extent(c(2135078, 2135478, -93158, -92758))
+# crop_extent = extent(c(xxx, xxx, xxx, xxx))
 
 # # For a 5 x 5 landscape
-# crop_extent = extent(c(2135078, 2135178, -93158, -93057))
+# crop_extent = extent(c(xxx, xxx, xxx, xxx))
 #! --- End crash test zone, to have a smaller landscape
 
 croppedClimate = crop(croppedClimate, crop_extent)
@@ -134,15 +147,19 @@ croppedClimate = crop(croppedClimate, crop_extent)
 (nrows = nrow(croppedClimate))
 (ncols = ncol(croppedClimate))
 
-deltaX = xres(croppedClimate) # 20
-deltaY = yres(croppedClimate) # 20.10989
+deltaX = xres(croppedClimate)
+deltaY = yres(croppedClimate)
 
 ## Get centroid and in which cell it belongs
-centroid = colMeans(coordinates(croppedClimate))
+coords = crds(croppedClimate)
+centroid = colMeans(coords)
 
 ## Coerce to data table
-vals = as.data.table(rasterToPoints(croppedClimate))
-setnames(vals, old = c("x", "y"), new = c("longitude", "latitude"))
+vals = as.data.table(as.points(x = croppedClimate, values = TRUE, na.rm = TRUE))
+if (vals[, .N] != nrows*ncols)
+	warning("You probably have NAs in your raster. Check it, as this brings problems later on...")
+
+vals[, c("longitude", "latitude") := split(coords, rep(1:ncol(coords), each = nrow(coords)))]
 
 ## If homogeneous climate, compute average
 if (homogeneousClimate)
@@ -175,6 +192,9 @@ vals[row %in% seq(max(row) - 9, max(row)), isPopulated := "true"]
 
 # Populations are at the 20 bottom lines of the landscape
 vals[row %in% seq(max(row) - 19, max(row)), isPopulated := "true"]
+
+# Populations are at the 30 bottom lines of the landscape #! i.e., 10% of the 300*11 landscape, starting from the bottom
+vals[row %in% seq(max(row) - 29, max(row)), isPopulated := "true"]
 
 # Populations are at the 100 top lines of the landscape
 vals[row %in% seq(min(row), min(row) + 99), isPopulated := "true"]
@@ -216,20 +236,20 @@ if (refugiaOption)
 
 #### Save files
 ## Environment files for C++ prog
-vals[, writeCppClimate_DT(unlist(vals[rowNumber]), cppNames, patch_id, crs, deltaX, deltaY, path = outputPath, sep = " = ", rm = TRUE), by = rowNumber]
+vals[, writeCppClimate_DT(unlist(vals[rowNumber]), cppNames, patch_id, crs_str, deltaX, deltaY, path = outputPath, sep = " = ", rm = TRUE), by = rowNumber]
 
 ## Id files of populated patches (to create the Initial Condition in createIC folder)
 # List occupied patches
 patch_id = vals[isPopulated == "true", patch_id]
 
 # Abies balsamea
-maxRow_abba = 180
+maxRow_abba = 270
 max_index_limitRow = (maxRow_abba - 1)*ncols + ncols - 1
 ls_id_abba = patch_id[patch_id <= max_index_limitRow]
 abba = data.table(patch_id = ls_id_abba, species = "Abies_balsamea")
 
 # Acer saccharum
-minRow_acsa = 181
+minRow_acsa = 271
 min_index_limitRow = (minRow_acsa - 1)*ncols
 ls_id_acsa = patch_id[patch_id >= min_index_limitRow]
 acsa = data.table(patch_id = ls_id_acsa, species = "Acer_saccharum")
@@ -244,7 +264,12 @@ saveRDS(patch_data, paste0(outputPath, "populatedPatches.rds"))
 ## Write data to create Matlab's data
 saveRDS(vals, paste0(outputPath, "climate.rds"))
 
-#### Plot to check centroid
+# #### Plot
+# pdf("test.pdf")
+# plot(croppedClimate, axes = FALSE, col = viridis::viridis(256))
+# dev.off()
+
+# #### Plot to check centroid
 # pdf("test.pdf", height = 8, width = 8)
 # croppedClimate = setValues(x = croppedClimate, values = sample(1:ncell(croppedClimate), ncell(croppedClimate), replace = TRUE), layer = 1)
 # plot(croppedClimate[["annual_mean_temperature"]])
